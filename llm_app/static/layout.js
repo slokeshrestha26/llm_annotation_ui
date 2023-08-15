ddc39de7cbba41bc35ab019249a5cd8e9c83f74c
Sloke Shrestha <slokeshrestha131@gmail.com>
2023-08-15 17:08:45 -0500

    got rid of chached responses and suggestions

---
+++ llm_app/static/layout.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Clear radio button selections
+    var radioButtons = document.querySelectorAll('input[type="radio"]');
+    radioButtons.forEach(function (radio) {
+      radio.checked = false;
+    });
+
+    var textFields = document.querySelectorAll('input[type="text"]');
+    textFields.forEach(function (textField) {
+      textField.autocomplete = "off";
+      textField.value = "";
+    });
+  });
